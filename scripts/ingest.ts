1d792ccb047447d54fc5b35b39c92896c8923f60
Md. Rakib <113634586+0xmdrakib@users.noreply.github.com>
2026-03-26 02:57:47 +0600

    Update ingest.ts

--- scripts/ingest.ts
+++ scripts/ingest.ts
@@ -1,3 +1,8 @@
+import nextEnv from "@next/env";
 import { ingestOnce } from "@/lib/ingest";
+
+const { loadEnvConfig } = nextEnv;
+loadEnvConfig(process.cwd());
+
 const res = await ingestOnce();
 console.log(res);
